06cb1ce317e73aea40ca52c022c1141342c13705
J. Peng <pengjun@imech.ac.cn>
2015-10-16 21:10:03 +0800

    add Xcode project

---
+++ nuc3d-mac/nuc3d-mac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nuc3d-mac
+//
+//  Created by Jun Peng on 15/10/16.
+//  Copyright © 2015年 Jun Peng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
