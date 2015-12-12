a996ad8946360a63b7c8de156177ccb2c973144c
pengjun62@gmail.com <pengjun62@gmail.com>
2015-12-12 17:10:08 +0800

    asm scheme

--- src/physicsModel.cpp
+++ src/physicsModel.cpp
@@ -467,7 +467,7 @@ double nuc3d::physicsModel::getMachL(const double &mach)
     double MachL;
     
     if (std::abs(mach) < 1.0)
-        MachL = 0.25*pow((mach + 1.0), 2) + 0.125*pow(pow(mach, 2) - 1.0, 2);
+        MachL = 0.25*pow((mach + 1.0), 2);
     else
         MachL = 0.50*(mach + std::abs(mach));
     
@@ -481,7 +481,7 @@ double nuc3d::physicsModel::getMachR(const double &mach)
     double MachR;
     
     if (std::abs(mach) < 1.0)
-        MachR = -0.25*pow((mach - 1.0), 2) - 0.125*pow(pow(mach, 2) - 1.0, 2);
+        MachR = -0.25*pow((mach - 1.0), 2);
     else
         MachR = 0.50*(mach - std::abs(mach));
     
@@ -490,9 +490,10 @@ double nuc3d::physicsModel::getMachR(const double &mach)
 }
 
 double nuc3d::physicsModel::getPressureL(const double &mach, const double &p)
-{    double pressureL;
+{
+    double pressureL;
     if (std::abs(mach) < 1.0)
-        pressureL = p*(0.25*pow((mach + 1.0), 2)*(2.0 - mach) + 0.1875*mach*pow(pow(mach, 2.0) - 1.0, 2));
+        pressureL = p*0.25*pow((mach + 1.0), 2)*(2.0 - mach);
     else
         pressureL = 0.50*p*(mach + std::abs(mach)) / mach;
     
@@ -503,7 +504,7 @@ double nuc3d::physicsModel::getPressureR(const double &mach, const double &p)
 {
     double pressureR;
     if (std::abs(mach) < 1.0)
-        pressureR = p*(0.25*pow(mach - 1.0, 2)*(2.0 + mach) - 0.1875*mach*pow(pow(mach, 2.0) - 1.0, 2));
+        pressureR = p*0.25*pow(mach - 1.0, 2)*(2.0 + mach);
     else
         pressureR = 0.5*p*(mach - std::abs(mach)) / mach;
     
