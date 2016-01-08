cb857999a43c11cd39129b479d91199b4191739a
pengjun62@gmail.com <pengjun62@gmail.com>
2016-01-09 01:13:13 +0800

    ...

--- src/boundaryConditions.cpp
+++ src/boundaryConditions.cpp
@@ -1534,7 +1534,7 @@ void nuc3d::boundaryCondition::VisBCsetter_wall_eta(PDEData3d &myPDE,
                 
                 double value=-u[idx];
                 
-                pRecv[idx_xi]=value;
+                pRecv[idx_xi]=0;
                 
             }
         }
@@ -1559,7 +1559,7 @@ void nuc3d::boundaryCondition::VisBCsetter_wall_eta(PDEData3d &myPDE,
                 
                 double value=-v[idx];
                 
-                pRecv[idx_xi]=value;
+                pRecv[idx_xi]=0;
                 
             }
         }
@@ -1584,7 +1584,7 @@ void nuc3d::boundaryCondition::VisBCsetter_wall_eta(PDEData3d &myPDE,
                 
                 double value=-w[idx];
                 
-                pRecv[idx_xi]=value;
+                pRecv[idx_xi]=0;
                 
             }
         }
