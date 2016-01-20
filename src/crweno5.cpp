85d9c82032f06ade7a027cc46f3cc68120db5673
pengjun62@gmail.com <pengjun62@gmail.com>
2016-01-20 15:48:45 +0800

    ...

--- src/crweno5.cpp
+++ src/crweno5.cpp
@@ -54,11 +54,6 @@ void nuc3d::crweno5::crweno5p(const Field & fieldIN,
     double is0,is1,is2;
     double q30,q31,q32;
     
-    double a[256];
-    double b[256];
-    double c[256];
-    double d[256];
-    
     int nx=fieldIN.getSizeX();
     int ny=fieldIN.getSizeY();
     int nz=fieldIN.getSizeZ();
@@ -66,6 +61,11 @@ void nuc3d::crweno5::crweno5p(const Field & fieldIN,
     int nx0=fieldOUT.getSizeX();
     int ny0=fieldOUT.getSizeY();
     int nz0=fieldOUT.getSizeZ();
+    
+    std::vector<double> a(nx-2*tilesize+1,0.0);
+    std::vector<double> b(nx-2*tilesize+1,1.0);
+    std::vector<double> c(nx-2*tilesize+1,0.0);
+    std::vector<double> d(nx-2*tilesize+1,0.0);
     
     int ibeg=(tilesize-1);
     int iend=nx-tilesize;
@@ -231,13 +231,12 @@ void nuc3d::crweno5::crweno5n(const Field & fieldIN,
     
     int nx=fieldIN.getSizeX();
     int ny=fieldIN.getSizeY();
-    int nz=fieldIN.getSizeZ();
+    int nz=fieldIN.getSizeZ();    
     
-    
-    double a[256];
-    double b[256];
-    double c[256];
-    double d[256];
+    std::vector<double> a(nx-2*tilesize+1,0.0);
+    std::vector<double> b(nx-2*tilesize+1,1.0);
+    std::vector<double> c(nx-2*tilesize+1,0.0);
+    std::vector<double> d(nx-2*tilesize+1,0.0);
 
     int nx0=fieldOUT.getSizeX();
     int ny0=fieldOUT.getSizeY();
